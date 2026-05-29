5144c27b952a83517e60b858b4d424c25107d0ab
Clément Benedetti <clement.benedetti@mri.cnrs.fr>
2026-05-29 10:23:24 +0200

    added the possibility to split a stack along the 'C' axis

--- src/set_calibration/__init__.py
+++ src/set_calibration/__init__.py
@@ -3,8 +3,11 @@ try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
-from ._widget import LayerScaleWidget
+
+from ._set_scale_widget import LayerScaleWidget
+from ._split_channels_widget import SplitChannelsWidget
 
 __all__ = (
-    "LayerScaleWidget"
+    "LayerScaleWidget",
+    "SplitChannelsWidget"
 )
---
+++ src/set_calibration/_split_channels_widget.py
@@ -0,0 +1,99 @@
+from napari.utils.notifications import (
+    show_info, 
+    show_warning
+)
+from autooptions import OptionsWidget
+from autooptions.options import Options
+from qtpy.QtWidgets import (
+    QWidget,
+    QVBoxLayout
+)
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import napari
+
+
+class SplitChannelsWidget(QWidget):
+    def __init__(self, viewer: "napari.viewer.Viewer"):
+        super().__init__()
+        self.viewer     = viewer
+        self.sameRowSet = set()
+        self.options    = self.getOptions()
+        self.operation  = None
+        self.widget     = self.createLayout()
+
+    def createLayout(self):
+        widget = OptionsWidget(
+            viewer=self.viewer, 
+            options=self.options, 
+            layout_type='vertical', 
+            client=self,
+            sameRowSet=self.sameRowSet
+        )
+        widget.addApplyButton(self.apply)
+        layout = QVBoxLayout()
+        layout.addWidget(widget)
+        self.setLayout(layout)
+        return widget
+
+    def getOptions(self):
+        options = Options("SetScaleTool", "SplitChannels")
+        options.addBool("Remove original?", value=True)
+        options.addBool("Apply to all", value=True)
+        options.load()
+        return options
+    
+    def _getTargetLayers(self):
+        if self.options.value("Apply to all"):
+            return self.viewer.layers
+        else:
+            active_layer = self.viewer.layers.selection.active
+            return [active_layer] if active_layer else []
+    
+    def removeAxis(self, v, index):
+        vector = list(v)
+        return vector[:index] + vector[index+1:]
+
+    def splitChannels(self, layer):
+        if 'C' not in layer.axis_labels:
+            show_info(f"'{layer.name}' does not have a 'C' axis.")
+            return False
+        
+        c_axis = layer.axis_labels.index('C')
+        data = layer.data
+        metadata = layer.metadata
+        name = layer.name
+
+        scale = self.removeAxis(layer.scale, c_axis)
+        units = self.removeAxis(layer.units, c_axis)
+        translate = self.removeAxis(layer.translate, c_axis)
+        axis_labels = self.removeAxis(layer.axis_labels, c_axis)
+
+        self.viewer.add_image(
+            data=data,
+            scale=scale,
+            units=units,
+            metadata=metadata,
+            translate=translate,
+            name=name,
+            channel_axis=c_axis,
+            blending='additive',
+            axis_labels=axis_labels
+        )
+        
+        return True
+    
+    def removeLayers(self, layer_names):
+        for name in layer_names:
+            if name in self.viewer.layers:
+                l = self.viewer.layers[name]
+                self.viewer.layers.remove(l)
+    
+    def apply(self):
+        layers = self._getTargetLayers()
+        toBeRemoved = set()
+        for layer in layers:
+            if self.splitChannels(layer):
+                toBeRemoved.add(layer.name)
+        self.removeLayers(toBeRemoved)
