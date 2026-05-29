cf89c2d02d1a54767b3b25716ab9d7cdffd05b78
Clément Benedetti <clement.benedetti@mri.cnrs.fr>
2026-05-29 15:24:50 +0200

    new features
    - apply to all as a button rather than a checkbox
    - filtered list of axes
    - label to show what is present already

--- src/set_calibration/_set_scale_widget.py
+++ src/set_calibration/_set_scale_widget.py
@@ -6,7 +6,8 @@ from autooptions import OptionsWidget
 from autooptions.options import Options
 from qtpy.QtWidgets import (
     QWidget,
-    QVBoxLayout
+    QVBoxLayout,
+    QLabel
 )
 from typing import TYPE_CHECKING
 
@@ -19,10 +20,12 @@ class LayerScaleWidget(QWidget):
         super().__init__()
         self.viewer     = viewer
         self.sameRowSet = set()
+        self.infoLabel  = QLabel("")
         self.options    = self.getOptions()
         self.operation  = None
         self.widget     = self.createLayout()
         self.viewer.layers.selection.events.changed.connect(self.onActiveLayerChanged)
+        self.onActiveLayerChanged(None)
 
     def createLayout(self):
         widget = OptionsWidget(
@@ -33,18 +36,16 @@ class LayerScaleWidget(QWidget):
             sameRowSet=self.sameRowSet
         )
         widget.addApplyButton(self.apply)
+        widget.addButton("Apply to all", self.applyToAll)
         layout = QVBoxLayout()
+        layout.addWidget(self.infoLabel)
         layout.addWidget(widget)
         self.setLayout(layout)
         return widget
-
-    def getOptions(self):
-        options = Options("SetScaleTool", "SetScaleAxes")
-        options.addFloat("X", value=1.0)
-        options.addFloat("Y", value=1.0)
-        options.addFloat("Z", value=1.0)
-        options.addChoice("Unit", choices=["nm", "µm", "mm", "cm", "m"], value="µm")
-        options.addChoice("Axes", choices=[
+    
+    @staticmethod
+    def getAxesPool():
+        return [
             "YX",
             "CYX",
             "YXC",
@@ -57,14 +58,36 @@ class LayerScaleWidget(QWidget):
             "TZYX",
             "TCZYX",
             "TZCYX"
-        ], value='YX')
-        options.addBool("Apply to all", value=True)
+        ]
+
+    def getOptions(self):
+        options = Options("SetScaleTool", "SetScaleAxes")
+        options.addFloat("X", value=1.0)
+        options.addFloat("Y", value=1.0)
+        options.addFloat("Z", value=1.0)
+        options.addChoice("Unit", choices=["pixels", "nm", "µm", "mm", "cm", "m"], value="µm")
+        options.addChoice("Axes", choices=self.getAxesPool(), value='YX')
         options.load()
         self.sameRowSet = {"Y", "Z"}
         return options
     
-    def _getTargetLayers(self):
-        if self.options.value("Apply to all"):
+    def updateAxesChoices(self):
+        _, w = self.widget.widgets.get("Axes", None)
+        if w is None:
+            return
+        
+        w.clear()
+        l = self.viewer.layers.selection.active
+        if l is None:
+            w.addItems(["---"])
+            return
+        
+        ndims = l.data.ndim
+        axes = [a for a in self.getAxesPool() if len(a) == ndims]
+        w.addItems(axes)
+    
+    def _getTargetLayers(self, to_all=False):
+        if to_all:
             return self.viewer.layers
         else:
             active_layer = self.viewer.layers.selection.active
@@ -73,6 +96,7 @@ class LayerScaleWidget(QWidget):
     def onActiveLayerChanged(self, event):
         self.showCurrent()
         self.updateViewersAxisLabels()
+        self.updateAxesChoices()
         
     def showCurrent(self):
         l = self.viewer.layers.selection.active
@@ -80,8 +104,8 @@ class LayerScaleWidget(QWidget):
             return
         axes = l.axis_labels
         scales = l.scale
-        as_str = ", ".join(f"{a}: {s:.2f}" for a, s in zip(axes, scales))
-        show_info(as_str)
+        as_str =  "   |   ".join(f"'{a}' ({i}): {s:.2f}" for i, (a, s) in enumerate(zip(axes, scales)))
+        self.infoLabel.setText(as_str)
 
     def updateViewersAxisLabels(self):
         layer = self.viewer.layers.selection.active
@@ -101,9 +125,12 @@ class LayerScaleWidget(QWidget):
     def makeUnitsVector(self, axes, unit):
         nonSpatialAxes = {'T', 'C'}
         return [unit if axis not in nonSpatialAxes else '' for axis in axes]
-    
-    def apply(self):
-        layers = self._getTargetLayers()
+
+    def applyToAll(self):
+        self.apply(to_all=True)
+
+    def apply(self, to_all=False):
+        layers = self._getTargetLayers(to_all)
         ax = list(self.options.value("Axes"))
         u = self.options.value("Unit")
         calib = {
@@ -129,6 +156,7 @@ class LayerScaleWidget(QWidget):
         
         self.updateScaleBar(u)
         self.updateViewersAxisLabels()
+        self.showCurrent()
     
     def updateScaleBar(self, unit):
         self.viewer.scale_bar.unit = unit
